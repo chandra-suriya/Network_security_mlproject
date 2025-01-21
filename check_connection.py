9320ebaffd66eea0064ccca135446154f2fb85b8
Chandra suriya <chandrasuriya305@outlook.com>
2025-01-21 13:44:01 +0530

    changes done in check_connection

--- check_connection.py
+++ check_connection.py
@@ -1,15 +1,25 @@
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://chandrasuriya007:Chan123@cluster0.pnwcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# uri = "--- add your mongodb atlas url ----"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+# # Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
+
+from networksecurity.utils.main_utils.utils import read_yaml_file, write_yaml_file
+from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
 try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    data = read_yaml_file(SCHEMA_FILE_PATH)
+    print(len(data['numerical_columns']))
+
 except Exception as e:
     print(e)
+
